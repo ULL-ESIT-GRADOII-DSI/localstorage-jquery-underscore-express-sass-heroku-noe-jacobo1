f85c7c2cb043b403c7f1dca3cf785880d5f7c4a4
alu0100622492 <alu0100622492@ull.edu.es>
2016-04-11 03:18:21 +0100

    segimos con xpress y vistas añadidos nuevos paquetes

--- app/miapp.js
+++ app/miapp.js
@@ -1,22 +1,54 @@
 var express = require('express')
-var app = express()
+  , path = require('path');
+
+
 var _ = require('underscore');
+var app = express()
+var router = express.Router();
+
+/*
+Las funciones de middleware pueden realizar las siguientes tareas:
+
+Ejecutar cualquier código.
+Realizar cambios en la solicitud y los objetos de respuesta.
+Finalizar el ciclo de solicitud/respuestas.
+Invocar la siguiente función de middleware en la pila
+*/
+
+
 // https://nodejs.org/api/path.html
-var path = require('path');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
-// set the view engine to ejs
-app.set('view engine', 'ejs'); // http://expressjs.com/api.html#app.set
+//app.set('views', path.join(__dirname, '../assets/*'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 
+var expressLayouts = require('express-ejs-layouts');
+app.set('layout', 'layout'); // defaults to 'layout'  '
 
 // Serve static files
-app.use(express.static('.')); // http://expressjs.com/api.html#app.use#
+app.use(express.static('../views/layout')); // http://expressjs.com/api.html#app.use#
+app.set('view engine', 'ejs');
 
+//app.use(expressLayouts);
 
 // Luego la consultamos con app.get('port')
 app.set('port', (process.env.PORT || 8080));
+
+
+// define the home page route
+router.get('../assets/css/*', function(req, res) {
+  res.send('Birds home page');
+})
+// define the about route
+router.get('js', function(req, res) {
+  res.send('About birds');
+})//module exports al final ----------------------
+
+
 
 /*
  * body-parser is a piece of express middleware that
@@ -28,11 +60,18 @@ app.set('port', (process.env.PORT || 8080));
  */
 
  // instruct the app to use the `bodyParser()` middleware for all routes
-
-
+ var bodyParser = require('body-parser')
+ app.use(bodyParser.urlencoded({ extended: false }));
+ //app.use('/static', express.static(__dirname + '/public'));//poner rutas obasoluta a los directorios que se van a servir
+ app.use('css', express.static(__dirname + '../assets/css/*'));//prueba
  // A browser's default method is 'GET', so this
  // is the route that express uses when we visit
  // our site initially.
+
+
+
+
+
  app.get('/', function(req, res){
    // The form's action is '/' and its method is 'POST',
    // so the `app.post('/', ...` route will receive the
@@ -43,11 +82,13 @@ app.set('port', (process.env.PORT || 8080));
  // This route receives the posted form.
  // As explained above, usage of 'body-parser' means
  // that `req.body` will be filled in with the form elements
+ /*
  app.post('/', function(req, res){
    var userName = req.body.userName;
-   res.render('index', {userName: userName, title: 'greet'});
- });
+   res.render('inde', {userName: userName, title: 'greet'});
+ });*/
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+module.exports = router;
